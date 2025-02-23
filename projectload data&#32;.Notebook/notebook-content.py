d314eedc6f2ae00ccc1295af7e09436c9e5d9fba
fabrictrainer1 <fabrictrainer@ytta.in>
2025-02-23 20:51:17 +0530

    Committing 1 item from workspace b05d5296-699b-4751-81ea-8628404bee3d

---
+++ projectload data&#32;.Notebook/notebook-content.py
@@ -0,0 +1,20 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "6d60def4-35ca-41ce-9d45-93e3e1f9a44e",
+# META       "default_lakehouse_name": "wslh1",
+# META       "default_lakehouse_workspace_id": "b05d5296-699b-4751-81ea-8628404bee3d"
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# #working as sample
